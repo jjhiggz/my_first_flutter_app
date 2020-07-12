680411be6ab768d69177341bca780cbb3338b04a
jjhiggz <jonathan.higger@gmail.com>
2020-07-11 23:41:50 -0600

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // this is where the UI rendering work that kicks off the screen goes
-  runApp(MyApp());
-}
+// this is where the UI rendering work that kicks off the screen goes
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
+  @override //makes it clear that we are overriding the build widget for My app with the build wideget for our child widget
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello!'),
