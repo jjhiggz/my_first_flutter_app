6fbc09d32ddb388b4592b97790f72d5700d54a09
jjhiggz <jonathan.higger@gmail.com>
2020-07-12 09:22:21 -0600

    turning classes and class methods on the main dart file into private classes / class methods by using the _ in front of their names which is a dart convention

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,28 @@ import 'package:flutter/material.dart';
 // this is where the UI rendering work that kicks off the screen goes
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override //makes it clear that we are overriding the build widget for My app with the build wideget for our child widget
-  Widget build(BuildContext context) {
-    void answerQuestion(int answerNumber) {
-      print(answerNumber);
-    }
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion(int answerNumber) {
+    print(_questionIndex);
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override //makes it clear that we are overriding the build widget for My app with the build wideget for our child widget
+
+  Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
       'What\'s your favorite animal>'
@@ -21,18 +36,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question'),
+            Text(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: () => answerQuestion(1),
+              onPressed: () => _answerQuestion(1),
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: () => answerQuestion(2),
+              onPressed: () => _answerQuestion(2),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: () => answerQuestion(3),
+              onPressed: () => _answerQuestion(3),
             ),
           ],
         ),
