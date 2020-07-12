7d9ffc91e0c14410e7f848ace018859e9a80ce45
jjhiggz <jonathan.higger@gmail.com>
2020-07-11 23:48:47 -0600

    scaffolded app, added nav bar and body

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 // this is where the UI rendering work that kicks off the screen goes
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override //makes it clear that we are overriding the build widget for My app with the build wideget for our child widget
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text'),
+      ),
     ); // does base set up that turns base combination into a full app
   }
 }
