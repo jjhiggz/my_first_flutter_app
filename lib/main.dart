b00966b903f48f2057ef1e475e976f7453ebf8d7
jjhiggz <jonathan.higger@gmail.com>
2020-07-12 00:15:49 -0600

    increased button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override //makes it clear that we are overriding the build widget for My app with the build wideget for our child widget
   Widget build(BuildContext context) {
+    void answerQuestion(int answerNumber) {
+      print(answerNumber);
+    }
+
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal>'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(1),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => answerQuestion(2),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => answerQuestion(3),
+            ),
+          ],
+        ),
       ),
     ); // does base set up that turns base combination into a full app
   }
